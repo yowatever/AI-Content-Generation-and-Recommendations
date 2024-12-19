75f6a7e8378c1ff8a32dd17ae4c4ab9e49bf865a
yowatever <ahmed.afifa07@gmail.com>
2024-12-20 02:50:23 +0530

    a

---
+++ app/models/generate_model.py
@@ -0,0 +1,25 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from langchain.llms import HuggingFaceLLM
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+
+class ContentGenerator:
+    def __init__(self, model_name="nisten/Biggie-SmoLlm-0.15B-Base"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.llm = HuggingFaceLLM(
+            model=self.model,
+            tokenizer=self.tokenizer
+        )
+        self.chain = self._create_chain()
+    
+    def _create_chain(self):
+        prompt = PromptTemplate(
+            template="Write a creative story based on: {prompt}",
+            input_variables=["prompt"]
+        )
+        return LLMChain(llm=self.llm, prompt=prompt)
+    
+    def generate(self, prompt):
+        """Generate creative content based on the prompt"""
+        return self.chain.run(prompt=prompt)
