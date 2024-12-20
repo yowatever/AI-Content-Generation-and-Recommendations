bc85a501a9886ca7ecf79343294b26f68de5181b
yowatever <ahmed.afifa07@gmail.com>
2024-12-20 11:26:38 +0530

    a

--- app/models/generate_model.py
+++ app/models/generate_model.py
@@ -1,16 +1,24 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from langchain.llms import HuggingFacePipeline
+from langchain_community.llms import HuggingFacePipeline
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from transformers import pipeline
 
 class ContentGenerator:
     def __init__(self, model_name="nisten/Biggie-SmoLlm-0.15B-Base"):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
-        self.llm = HuggingFaceLLM(
+        
+        # Create pipeline
+        pipe = pipeline(
+            "text-generation",
             model=self.model,
-            tokenizer=self.tokenizer
+            tokenizer=self.tokenizer,
+            max_length=100
         )
+        
+        # Create LLM
+        self.llm = HuggingFacePipeline(pipeline=pipe)
         self.chain = self._create_chain()
     
     def _create_chain(self):
